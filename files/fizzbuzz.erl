deb01c36d838d9da9c837ed65759f1e7451143ea
Z4EHgd <Z4EHgd@cyber-dojo.org>
2022-06-29 21:26:42 +0000

    19 ran tests, no prediction, got green

--- files/fizzbuzz.erl
+++ files/fizzbuzz.erl
@@ -4,5 +4,5 @@
 answer(15) -> [1,2,"Fizz",4,"Buzz","Fizz",7,8,"Fizz","Buzz",11,"Fizz",13,14,"FizzBuzz"];
 answer(5) -> [1,2,"Fizz",4,"Buzz"].
 
-convert(N) when N % 3 =:= 0 -> "Fizz";
+convert(N) when N rem 3 == 0 -> "Fizz";
 convert(5) -> "Buzz".
