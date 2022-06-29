d4096eacf537bafa71b350c9a1452c313f4e93da
Z4EHgd <Z4EHgd@cyber-dojo.org>
2022-06-29 21:17:06 +0000

    9 ran tests, no prediction, got green

--- files/fizzbuzz.erl
+++ files/fizzbuzz.erl
@@ -2,4 +2,4 @@
 -export([answer/1]).
 
 answer(15) -> [1,2,"Fizz",4,"Buzz","Fizz",7,8,"Fizz","Buzz",11,"Fizz",13,14,"FizzBuzz"];
-      (5) -> [1,2,"Fizz",4,"Buzz"].
+answer(5) -> [1,2,"Fizz",4,"Buzz"].
