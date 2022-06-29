e14fd3c58541ecaa7c34b41f75325b66a1e4cffe
Z4EHgd <Z4EHgd@cyber-dojo.org>
2022-06-29 21:22:44 +0000

    14 ran tests, no prediction, got amber

--- files/fizzbuzz.erl
+++ files/fizzbuzz.erl
@@ -4,4 +4,5 @@
 answer(15) -> [1,2,"Fizz",4,"Buzz","Fizz",7,8,"Fizz","Buzz",11,"Fizz",13,14,"FizzBuzz"];
 answer(5) -> [1,2,"Fizz",4,"Buzz"].
 
-convert(_) -> "Fizz".
+convert(3) -> "Fizz";
+convert(5) -> "Buzz".
