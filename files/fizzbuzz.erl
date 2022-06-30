3fd82fec82d8b70a47014a97216d5371c8fc2302
Z4EHgd <Z4EHgd@cyber-dojo.org>
2022-06-30 12:34:11 +0000

    27 ran tests, no prediction, got red

--- files/fizzbuzz.erl
+++ files/fizzbuzz.erl
@@ -4,7 +4,7 @@
 answer(N) -> answer(1, N, []).
 
 answer(Max, Max, Result) -> Result;
-answer(N, Max, Result) -> answer(N+1, Max, Result || convert(N)).
+answer(N, Max, Result) -> answer(N+1, Max, [Result|convert(N)]).
 
 convert(N) when N rem 15 == 0 -> "FizzBuzz";
 convert(N) when N rem 3 == 0 -> "Fizz";
