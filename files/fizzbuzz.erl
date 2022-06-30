936afddd2ee213dbac6876b16889022d0d77ca8f
Z4EHgd <Z4EHgd@cyber-dojo.org>
2022-06-30 12:31:56 +0000

    26 ran tests, no prediction, got amber

--- files/fizzbuzz.erl
+++ files/fizzbuzz.erl
@@ -1,8 +1,10 @@
 -module(fizzbuzz).
 -export([answer/1, convert/1]).
 
-answer(15) -> [1,2,"Fizz",4,"Buzz","Fizz",7,8,"Fizz","Buzz",11,"Fizz",13,14,"FizzBuzz"];
-answer(5) -> [1,2,"Fizz",4,"Buzz"].
+answer(N) -> answer(1, N, []).
+
+answer(Max, Max, Result) -> Result;
+answer(N, Max, Result) -> answer(N+1, Max, Result || convert(N)).
 
 convert(N) when N rem 15 == 0 -> "FizzBuzz";
 convert(N) when N rem 3 == 0 -> "Fizz";
